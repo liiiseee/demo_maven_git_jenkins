4bd045bd3e39151a09069dbfa77d17d22da18d28
lise houblon <lisehoublon@gmail.com>
2020-05-06 09:45:21 +0200

    création du projet

---
+++ src/main/java/com/intiformation/jenkins/demo/Calculatrice.java
@@ -0,0 +1,26 @@
+package com.intiformation.jenkins.demo;
+
+public class Calculatrice {
+	
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int mul(int a, int b) {
+		return a*b;
+	}
+
+}
---
+++ src/test/java/com/intiformation/jenkins/demo/CalculatriceTest.java
@@ -0,0 +1,26 @@
+package com.intiformation.jenkins.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author IN-DF-029
+ *
+ */
+public class CalculatriceTest {
+
+	@Test
+	public void testAdd() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est-ce que l'addition est correcte", 10, cal.add(5, 5));
+	}
+
+	@Test
+	public void testMul() {
+		Calculatrice cal = new Calculatrice();
+		assertEquals("Est-ce que la multiplication est correcte", 10, cal.mul(5, 2));
+	}
+
+}
